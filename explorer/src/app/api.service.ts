c7f117196cfb56fcb54e3fe57f3df545970df056
jonahrod7 <>
2019-12-03 20:43:52 +0100

    actualizacion

---
+++ explorer/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private endPoint = "https://api.worldbank.org/v2/country";
+  private format = "?per_page=1000&format=json";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCountries$() {
+    const url = this.endPoint + this.format;
+    return this.httpClient.get<any[]>(url).pipe(map(result => result[1]));
+  }
+}
